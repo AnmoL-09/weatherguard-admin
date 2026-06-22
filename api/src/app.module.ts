88427c9aef67bb8cf58b65b4bbe6fe1b86f0acaf
AnmoL-09 <anmolmahobiya092@gmail.com>
2026-06-22 21:42:46 +0530

    feat: configure mongodb and project modules

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,43 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { validationSchema } from './config/env.validation';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { AdminModule } from './admin/admin.module';
+import { TelegramModule } from './telegram/telegram.module';
+import { AlertsModule } from './alerts/alerts.module';
+import { WeatherModule } from './weather/weather.module';
+import { CronModule } from './cron/cron.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema,
+    }),
+
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+    }),
+
+    AuthModule,
+
+    UsersModule,
+
+    AdminModule,
+
+    TelegramModule,
+
+    AlertsModule,
+
+    WeatherModule,
+
+    CronModule,
+  ],
 })
 export class AppModule {}
